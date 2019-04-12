77f13db89ecc180c52aeadf11467ed440e3d50f0
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-12 23:36:13 +0200

    WIP shaving

---
+++ lib/energy_tree/shaving.ex
@@ -0,0 +1,5 @@
+defmodule EnergyTree.Shaving do
+  @moduledoc """
+  Handler for logic related to actual moment-to-moment peak shaving.
+  """
+end
---
+++ lib/energy_tree/shaving/dispatcher.ex
@@ -0,0 +1,26 @@
+defmodule EnergyTree.Shaving.Dispatcher do
+
+  def refine(list_of_usages, prosumers) do
+    aggregate = calculate_aggregate(list_of_usages)
+    if should_shave?(aggregate) do
+      ask_some_prosumers_to_produce(prosumers)
+    else
+      forward_aggregate_to_parent(aggregate)
+    end
+  end
+
+  def should_shave(aggregate) do
+    # TODO
+  end
+
+  def calculate_aggregate(list_of_usages) do
+    # TODO
+  end
+
+  def forward_aggregate_to_parent(aggregate) do
+    # TODO
+  end
+
+  def ask_prosumers_to_produce(prosumers) do
+  end
+end
