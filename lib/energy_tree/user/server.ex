2689ae646a8f09ab1299ce45db3faa2b651a6077
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-12 22:29:25 +0200

    Persistence works

--- lib/energy_tree/user/server.ex
+++ lib/energy_tree/user/server.ex
@@ -7,7 +7,7 @@ defmodule EnergyTree.User.Server do
   defstart start_link(user_id) do
     user = case Persistence.load_user(user_id) do
       {:ok, user} -> user
-      :error -> User.Struct.new
+      :error -> User.Struct.new(user_id: user_id)
     end
     initial_state({user_id, user})
   end
@@ -19,6 +19,13 @@ defmodule EnergyTree.User.Server do
       Persistence.persist_user(new_user)
     end)
     new_state({user_id, new_user})
+  end
+
+  defcast persist(), state: {user_id, user} do
+    Task.start(fn ->
+      Persistence.persist_user(user)
+    end)
+    noreply({user_id, user})
   end
 
   defcast stop, do: stop_server(:normal)
