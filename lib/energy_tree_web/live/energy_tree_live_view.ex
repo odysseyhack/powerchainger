d93227429e64df6d64ad2d9cbc03cece29b6f274
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-12 12:15:10 +0200

    Properly display time

--- lib/energy_tree_web/live/energy_tree_live_view.ex
+++ lib/energy_tree_web/live/energy_tree_live_view.ex
@@ -4,18 +4,18 @@ defmodule EnergyTreeWeb.EnergyTreeLiveView do
   def render(assigns) do
     ~L"""
     <h1> <%= assigns.title %></h1>
-    Current time: <%= inspect assigns.time %>
+    Current time: <%= assigns.time %>
     """
   end
 
   def mount(_session, socket) do
     schedule()
-    {:ok, assign(socket, val: 72, mode: :cooling, time: :calendar.local_time(), title: "Powerchainger")}
+    {:ok, assign(socket, val: 72, mode: :cooling, time: NaiveDateTime.utc_now, title: "Powerchainger")}
   end
 
   def handle_info(:tick, socket) do
     schedule()
-    {:noreply, assign(socket, time: :calendar.local_time())}
+    {:noreply, assign(socket, time: NaiveDateTime.utc_now)}
   end
 
   defp schedule() do
