481916a29affade036c2b83b6e2b059466d21ec5
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-13 10:57:32 +0200

    charging mode WIP

--- lib/energy_tree/user/struct.ex
+++ lib/energy_tree/user/struct.ex
@@ -4,4 +4,8 @@ defmodule EnergyTree.User.Struct do
   def new(map \\ %{}) do
     struct(__MODULE__, map)
   end
+
+  defmodule Dashboard do
+    defstruct []
+  end
 end
--- lib/energy_tree_web/live/energy_tree_live_view.ex
+++ lib/energy_tree_web/live/energy_tree_live_view.ex
@@ -4,6 +4,7 @@ defmodule EnergyTreeWeb.EnergyTreeLiveView do
   defstruct page: :dashboard, user: EnergyTree.User.Struct.new, show_menu?: false
 
   @pages [:dashboard, :profile, :settings, :sign_out]
+  @charging_modes [:charging, :saving]
 
   @impl true
   def render(assigns) do
@@ -36,28 +37,43 @@ defmodule EnergyTreeWeb.EnergyTreeLiveView do
 
   @impl true
   def handle_event(event, value, socket) do
-    case {event, value, socket} do
-      {"changed_name", %{"q" => new_name}, socket} ->
-        {user_id, user} = EnergyTree.User.Server.inspect
-        user = user
-          |> Map.put(:name, new_name)
-
-        EnergyTree.User.Server.set(user)
-        socket = assign(socket, title: new_name, user: user)
-        {:noreply, socket}
-      {"toggle_menu", _, socket} ->
-        IO.inspect("Toggled menu!")
-        socket = assign(socket, show_menu?: !socket.assigns.show_menu?)
-        {:noreply, socket}
-      {"change_page", page, socket} ->
-        page = String.to_existing_atom(page)
-        if page in @pages do
-          socket = assign(socket, page: page)
-          {:noreply, socket}
-        else
-          {:noreply, socket}
-        end
+    socket =
+      case {event, value, socket} do
+        {"changed_name", %{"q" => new_name}, socket} ->
+          change_name(socket, new_name)
+        {"toggle_menu", _, socket} ->
+          toggle_menu(socket)
+        {"change_page", page, socket} ->
+          change_page(socket, String.to_existing_atom(page))
+        {"toggle_charging_mode", charging_mode, socket} ->
+          toggle_charging_mode(socket, String.to_existing_atom(charging_mode))
     end
+    {:noreply, socket}
+  end
+
+  defp change_name(socket, new_name) do
+    {user_id, user} = EnergyTree.User.Server.inspect
+    user =
+      user
+      |> Map.put(:name, new_name)
+
+    EnergyTree.User.Server.set(user)
+    assign(socket, title: new_name, user: user)
+  end
+
+  defp toggle_menu(socket) do
+    socket
+    |> assign(show_menu?: !socket.assigns.show_menu?)
+  end
+
+  defp change_page(socket, page) when page in @pages do
+    socket
+    |> assign(page: page)
+  end
+
+  defp toggle_charging_mode(socket, charging_mode) when charging_mode in @charging_modes do
+    socket
+    |> assign(charging_mode: charging_mode)
   end
 
   defp schedule() do
