62176ed7bb35af0dc4e0909772b9b5a84e84bce6
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2019-04-14 01:53:08 +0200

    Helper for circular chart

--- lib/energy_tree_web/helpers.ex
+++ lib/energy_tree_web/helpers.ex
@@ -1,3 +1,20 @@
 defmodule EnergyTreeWeb.Helpers do
+  use Phoenix.HTML
+
+  def circle_chart(value, percentage, label, sublabel, classes, sublabel_classes) do
+    ~E"""
+    <svg viewBox="0 0 36 36" class="c-circular-chart <%= classes |> Enum.join(" ") %>">
+    <path class="c-circular-chart__bg" d="M18 2.0845
+    a 15.9155 15.9155 0 0 1 0 31.831
+    a 15.9155 15.9155 0 0 1 0 -31.831" />
+    <path class="c-circular-chart__fill" stroke-dasharray="<%= percentage %>, 100" d="M18 2.0845
+    a 15.9155 15.9155 0 0 1 0 31.831
+    a 15.9155 15.9155 0 0 1 0 -31.831" />
+    <text x="18" y="18.35" class="c-circular-chart__percentage"><%= value %></text>
+    <text x="18" y="24" class="c-circular-chart__label"><%= label %></text>
+    <text x="18" y="27.3" class="c-circular-chart__sublabel <%= sublabel_classes |> Enum.join(" ") %>"><%= sublabel %></text>
+    </svg>
+    """
+  end
 end
 
